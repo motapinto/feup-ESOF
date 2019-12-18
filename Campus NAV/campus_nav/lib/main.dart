6bba8a265261f9073e2fdfd5ff4e4f9a0f744479
Martim de Carvalho e Sousa Pinto da Silva <martimpintodasilva@gmail.com>
2019-12-18 12:56:31 +0000

    esof updated

--- Campus NAV/campus_nav/lib/main.dart
+++
@@ -1,8 +0,0 @@
-//Flutter widgets
-import 'package:flutter/material.dart';
-//Campus NAV start screen
-import 'package:campus_nav/view/screens/CampusNAV_screen.dart';
-
-
-
-void main() => runApp(CampusNAV());
--- Campus NAV/campus_nav/lib/view/Router/RouterConstants.dart
+++
@@ -1,8 +0,0 @@
-/// This file contains all the routing constants used within the app
-const String homeRoute = '/';
-const String profileRoute = '/profile';
-const String favouritesRoute = '/favourites';
-const String searchRoute = '/search';
-const String settingsRoute = '/settings';
-const String helpRoute = '/help';
-const String mapRoute = '/map';
